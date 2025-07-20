96c244433906fa9163ed86421053505909aeaf9a
khatiainanashvili <118260235+khatiainanashvili@users.noreply.github.com>
2025-07-20 11:45:08 +0400

    Update script.js

--- checkboxes/script.js
+++ checkboxes/script.js
@@ -2,7 +2,7 @@ const toggles =document.querySelectorAll('.toggle')
 const good = document.getElementById('good')
 const cheap = document.getElementById('cheap')
 const fast = document.getElementById('fast')
-
+const free = document.getElementById('free')
 toggles.forEach(toggle => toggle.addEventListener('change', (e) => toTheTrick(e.target)))
 
 function toTheTrick(theClickedOne) {
@@ -17,6 +17,11 @@ function toTheTrick(theClickedOne) {
 
         if (fast === theClickedOne) {
             cheap.checked = false
+        }
+           if (free === theClickedOne) {
+            cheap.checked = false
+            good.checked = false
+            fast.checked = false
         }
     }
 }
